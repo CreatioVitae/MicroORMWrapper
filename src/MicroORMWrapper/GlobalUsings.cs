54b2cdb08539e8e72c2a2f3f75b256829f91c525
CreatioVitae <h.chiba201103@gmail.com>
2021-11-16 21:34:24 +0900

    update .NET 6

---
+++ src/MicroORMWrapper/GlobalUsings.cs
@@ -0,0 +1,6 @@
+global using Dapper;
+global using System;
+global using System.Collections.Generic;
+global using System.Data;
+global using System.Data.Common;
+global using System.Threading.Tasks;
--- src/MicroORMWrapper/SqlManager.cs
+++ src/MicroORMWrapper/SqlManager.cs
@@ -1,179 +1,172 @@
-using Dapper;
-using System;
-using System.Collections.Generic;
-using System.Data;
-using System.Data.Common;
-using System.Threading.Tasks;
+namespace MicroORMWrapper;
 
-namespace MicroORMWrapper {
-    public class SqlManager<TDatabaseConnection> : IAsyncDisposable where TDatabaseConnection : IDatabaseConnection {
-        public DbConnection DbConnection { get; }
+public class SqlManager<TDatabaseConnection> : IAsyncDisposable where TDatabaseConnection : IDatabaseConnection {
+    public DbConnection DbConnection { get; }
 
-        DbTransaction? DbTransaction { get; set; }
+    DbTransaction? DbTransaction { get; set; }
 
-        public string ConnectionName { get; }
+    public string ConnectionName { get; }
 
-        public bool IsOpenedConnection => DbConnection.State == ConnectionState.Open;
+    public bool IsOpenedConnection => DbConnection.State == ConnectionState.Open;
 
-        public SqlManager(IDatabaseConnection databaseConnection) {
-            DbConnection = databaseConnection.DbConnection;
-            ConnectionName = databaseConnection.ConnectionName;
+    public SqlManager(IDatabaseConnection databaseConnection) {
+        DbConnection = databaseConnection.DbConnection;
+        ConnectionName = databaseConnection.ConnectionName;
 
-            OpenConnection();
+        OpenConnection();
+    }
+
+    public void OpenConnection() {
+        if (IsOpenedConnection) {
+            return;
         }
 
-        public void OpenConnection() {
-            if (IsOpenedConnection) {
-                return;
-            }
+        DbConnection.Open();
+    }
 
-            DbConnection.Open();
+    public async ValueTask BeginTransactionAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted) =>
+        DbTransaction = await DbConnection.BeginTransactionAsync(isolationLevel);
+
+    public async ValueTask<ScopedTransaction> BeginScopedTransactionAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted) {
+        DbTransaction = await DbConnection.BeginTransactionAsync(isolationLevel);
+
+        return new ScopedTransaction(DbTransaction);
+    }
+
+    public async ValueTask CommitAsync() {
+        if (DbTransaction.IsInvalid()) {
+            return;
         }
 
-        public async ValueTask BeginTransactionAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted) =>
-            DbTransaction = await DbConnection.BeginTransactionAsync(isolationLevel);
+        await DbTransaction.CommitAsync();
+    }
 
-        public async ValueTask<ScopedTransaction> BeginScopedTransactionAsync(IsolationLevel isolationLevel = IsolationLevel.ReadCommitted) {
-            DbTransaction = await DbConnection.BeginTransactionAsync(isolationLevel);
-
-            return new ScopedTransaction(DbTransaction);
+    public async ValueTask RollbackIfUncommitedAsync() {
+        if (DbTransaction.IsInvalid()) {
+            return;
         }
 
-        public async ValueTask CommitAsync() {
-            if (DbTransaction.IsInvalid()) {
-                return;
-            }
+        await DbTransaction.RollbackAsync();
+    }
 
-            await DbTransaction.CommitAsync();
+    DbTransaction? GetDbTransactionIfIsBegun() =>
+        DbTransaction.IsInvalid() ? null : DbTransaction;
+
+    void DisposeTransaction() {
+        if (DbTransaction.IsInvalid()) {
+            return;
         }
 
-        public async ValueTask RollbackIfUncommitedAsync() {
-            if (DbTransaction.IsInvalid()) {
-                return;
-            }
+        DbTransaction.Rollback();
+        DbTransaction.Dispose();
+    }
 
-            await DbTransaction.RollbackAsync();
+    async ValueTask DisposeTransactionAsync() {
+        if (DbTransaction.IsInvalid()) {
+            return;
         }
 
-        DbTransaction? GetDbTransactionIfIsBegun() =>
-            DbTransaction.IsInvalid() ? null : DbTransaction;
+        await DbTransaction.RollbackAsync();
+        await DbTransaction.DisposeAsync();
+    }
 
-        void DisposeTransaction() {
-            if (DbTransaction.IsInvalid()) {
-                return;
-            }
-
-            DbTransaction.Rollback();
-            DbTransaction.Dispose();
+    public void CloseConnection() {
+        if (!IsOpenedConnection) {
+            return;
         }
 
-        async ValueTask DisposeTransactionAsync() {
-            if (DbTransaction.IsInvalid()) {
-                return;
-            }
+        DbConnection.Close();
+    }
 
-            await DbTransaction.RollbackAsync();
-            await DbTransaction.DisposeAsync();
+    public async ValueTask CloseConnectionAsync() {
+        if (!IsOpenedConnection) {
+            return;
         }
 
-        public void CloseConnection() {
-            if (!IsOpenedConnection) {
-                return;
-            }
+        await DbConnection.CloseAsync();
+    }
 
-            DbConnection.Close();
-        }
+    public IEnumerable<TResult> Select<TResult>(string query) =>
+        DbConnection.Query<TResult>(query, transaction: GetDbTransactionIfIsBegun());
 
-        public async ValueTask CloseConnectionAsync() {
-            if (!IsOpenedConnection) {
-                return;
-            }
+    public IEnumerable<TResult> Select<TResult>(string query, object prameters) =>
+        DbConnection.Query<TResult>(query, prameters, transaction: GetDbTransactionIfIsBegun());
 
-            await DbConnection.CloseAsync();
-        }
+    public IEnumerable<TResult> Select<TResult>((string query, object prameters) queryAndParameters) =>
+        DbConnection.Query<TResult>(queryAndParameters.query, queryAndParameters.prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public IEnumerable<TResult> Select<TResult>(string query) =>
-            DbConnection.Query<TResult>(query, transaction: GetDbTransactionIfIsBegun());
+    public IEnumerable<TResult> Select<TResult, TInclude1>(string query, Func<TResult, TInclude1, TResult> includeFunc, object prameters, string splitOn = "Id") =>
+        DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
 
-        public IEnumerable<TResult> Select<TResult>(string query, object prameters) =>
-            DbConnection.Query<TResult>(query, prameters, transaction: GetDbTransactionIfIsBegun());
+    public IEnumerable<TResult> Select<TResult, TInclude1, TInclude2>(string query, Func<TResult, TInclude1, TInclude2, TResult> includeFunc, object prameters, string splitOn = "Id") =>
+        DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
 
-        public IEnumerable<TResult> Select<TResult>((string query, object prameters) queryAndParameters) =>
-            DbConnection.Query<TResult>(queryAndParameters.query, queryAndParameters.prameters, transaction: GetDbTransactionIfIsBegun());
+    public Task<IEnumerable<TResult>> SelectAsync<TResult>(string query) =>
+        DbConnection.QueryAsync<TResult>(query, transaction: GetDbTransactionIfIsBegun());
 
-        public IEnumerable<TResult> Select<TResult, TInclude1>(string query, Func<TResult, TInclude1, TResult> includeFunc, object prameters, string splitOn = "Id") =>
-            DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
+    public Task<IEnumerable<TResult>> SelectAsync<TResult>(string query, object prameters) =>
+        DbConnection.QueryAsync<TResult>(query, prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public IEnumerable<TResult> Select<TResult, TInclude1, TInclude2>(string query, Func<TResult, TInclude1, TInclude2, TResult> includeFunc, object prameters, string splitOn = "Id") =>
-            DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
+    public Task<IEnumerable<TResult>> SelectAsync<TResult>((string query, object prameters) queryAndParameters) =>
+        DbConnection.QueryAsync<TResult>(queryAndParameters.query, queryAndParameters.prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public Task<IEnumerable<TResult>> SelectAsync<TResult>(string query) =>
-            DbConnection.QueryAsync<TResult>(query, transaction: GetDbTransactionIfIsBegun());
+    public Task<IEnumerable<TResult>> SelectAsync<TResult, TInclude1>(string query, Func<TResult, TInclude1, TResult> includeFunc, object prameters, string splitOn = "Id") =>
+        DbConnection.QueryAsync(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
 
-        public Task<IEnumerable<TResult>> SelectAsync<TResult>(string query, object prameters) =>
-            DbConnection.QueryAsync<TResult>(query, prameters, transaction: GetDbTransactionIfIsBegun());
+    public Task<IEnumerable<TResult>> SelectAsync<TResult, TInclude1, TInclude2>(string query, Func<TResult, TInclude1, TInclude2, TResult> includeFunc, object prameters, string splitOn = "Id") =>
+        DbConnection.QueryAsync(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
 
-        public Task<IEnumerable<TResult>> SelectAsync<TResult>((string query, object prameters) queryAndParameters) =>
-            DbConnection.QueryAsync<TResult>(queryAndParameters.query, queryAndParameters.prameters, transaction: GetDbTransactionIfIsBegun());
+    public List<TResult> SelectAsList<TResult>(string query) =>
+        DbConnection.Query<TResult>(query, transaction: GetDbTransactionIfIsBegun()).AsList();
 
-        public Task<IEnumerable<TResult>> SelectAsync<TResult, TInclude1>(string query, Func<TResult, TInclude1, TResult> includeFunc, object prameters, string splitOn = "Id") =>
-            DbConnection.QueryAsync(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
+    public List<TResult> SelectAsList<TResult>(string query, object prameters) =>
+        DbConnection.Query<TResult>(query, prameters, transaction: GetDbTransactionIfIsBegun()).AsList();
 
-        public Task<IEnumerable<TResult>> SelectAsync<TResult, TInclude1, TInclude2>(string query, Func<TResult, TInclude1, TInclude2, TResult> includeFunc, object prameters, string splitOn = "Id") =>
-            DbConnection.QueryAsync(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn);
+    public List<TResult> SelectAsList<TResult>((string query, object prameters) queryAndParameters) =>
+        DbConnection.Query<TResult>(queryAndParameters.query, queryAndParameters.prameters, transaction: GetDbTransactionIfIsBegun()).AsList();
 
-        public List<TResult> SelectAsList<TResult>(string query) =>
-            DbConnection.Query<TResult>(query, transaction: GetDbTransactionIfIsBegun()).AsList();
+    public List<TResult> SelectAsList<TResult, TInclude1>(string query, Func<TResult, TInclude1, TResult> includeFunc, object prameters, string splitOn = "Id") =>
+        DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn).AsList();
 
-        public List<TResult> SelectAsList<TResult>(string query, object prameters) =>
-            DbConnection.Query<TResult>(query, prameters, transaction: GetDbTransactionIfIsBegun()).AsList();
+    public List<TResult> SelectAsList<TResult, TInclude1, TInclude2>(string query, Func<TResult, TInclude1, TInclude2, TResult> includeFunc, object prameters, string splitOn = "Id") =>
+        DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn).AsList();
 
-        public List<TResult> SelectAsList<TResult>((string query, object prameters) queryAndParameters) =>
-            DbConnection.Query<TResult>(queryAndParameters.query, queryAndParameters.prameters, transaction: GetDbTransactionIfIsBegun()).AsList();
+    public BuiltInType GetValue<BuiltInType>(string query) =>
+        DbConnection.ExecuteScalar<BuiltInType>(query, transaction: GetDbTransactionIfIsBegun());
 
-        public List<TResult> SelectAsList<TResult, TInclude1>(string query, Func<TResult, TInclude1, TResult> includeFunc, object prameters, string splitOn = "Id") =>
-            DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn).AsList();
+    public BuiltInType GetValue<BuiltInType>(string query, object prameters) =>
+        DbConnection.ExecuteScalar<BuiltInType>(query, prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public List<TResult> SelectAsList<TResult, TInclude1, TInclude2>(string query, Func<TResult, TInclude1, TInclude2, TResult> includeFunc, object prameters, string splitOn = "Id") =>
-            DbConnection.Query(query, includeFunc, prameters, transaction: GetDbTransactionIfIsBegun(), true, splitOn).AsList();
+    public Task<BuiltInType> GetValueAsync<BuiltInType>(string query) =>
+        DbConnection.ExecuteScalarAsync<BuiltInType>(query, transaction: GetDbTransactionIfIsBegun());
 
-        public BuiltInType GetValue<BuiltInType>(string query) =>
-            DbConnection.ExecuteScalar<BuiltInType>(query, transaction: GetDbTransactionIfIsBegun());
+    public Task<BuiltInType> GetValueAsync<BuiltInType>(string query, object prameters) =>
+        DbConnection.ExecuteScalarAsync<BuiltInType>(query, prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public BuiltInType GetValue<BuiltInType>(string query, object prameters) =>
-            DbConnection.ExecuteScalar<BuiltInType>(query, prameters, transaction: GetDbTransactionIfIsBegun());
+    public int Execute(string command) =>
+        DbConnection.Execute(command, transaction: GetDbTransactionIfIsBegun());
 
-        public Task<BuiltInType> GetValueAsync<BuiltInType>(string query) =>
-            DbConnection.ExecuteScalarAsync<BuiltInType>(query, transaction: GetDbTransactionIfIsBegun());
+    public int Execute(string command, object prameters) =>
+        DbConnection.Execute(command, prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public Task<BuiltInType> GetValueAsync<BuiltInType>(string query, object prameters) =>
-            DbConnection.ExecuteScalarAsync<BuiltInType>(query, prameters, transaction: GetDbTransactionIfIsBegun());
+    public Task<int> ExecuteAsync(string command) =>
+        DbConnection.ExecuteAsync(command, transaction: GetDbTransactionIfIsBegun());
 
-        public int Execute(string command) =>
-            DbConnection.Execute(command, transaction: GetDbTransactionIfIsBegun());
+    public Task<int> ExecuteAsync(string command, object prameters) =>
+        DbConnection.ExecuteAsync(command, prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public int Execute(string command, object prameters) =>
-            DbConnection.Execute(command, prameters, transaction: GetDbTransactionIfIsBegun());
+    public Task<int> ExecuteAsync((string command, object prameters) commandAndParameters) =>
+        DbConnection.ExecuteAsync(commandAndParameters.command, commandAndParameters.prameters, transaction: GetDbTransactionIfIsBegun());
 
-        public Task<int> ExecuteAsync(string command) =>
-            DbConnection.ExecuteAsync(command, transaction: GetDbTransactionIfIsBegun());
+    public void Dispose() {
+        DisposeTransaction();
+        CloseConnection();
+        GC.SuppressFinalize(this);
+    }
 
-        public Task<int> ExecuteAsync(string command, object prameters) =>
-            DbConnection.ExecuteAsync(command, prameters, transaction: GetDbTransactionIfIsBegun());
-
-        public Task<int> ExecuteAsync((string command, object prameters) commandAndParameters) =>
-            DbConnection.ExecuteAsync(commandAndParameters.command, commandAndParameters.prameters, transaction: GetDbTransactionIfIsBegun());
-
-        public void Dispose() {
-            DisposeTransaction();
-            CloseConnection();
-            GC.SuppressFinalize(this);
-        }
-
-        public async ValueTask DisposeAsync() {
-            await DisposeTransactionAsync();
-            await CloseConnectionAsync();
-            GC.SuppressFinalize(this);
-        }
+    public async ValueTask DisposeAsync() {
+        await DisposeTransactionAsync();
+        await CloseConnectionAsync();
+        GC.SuppressFinalize(this);
     }
 }
